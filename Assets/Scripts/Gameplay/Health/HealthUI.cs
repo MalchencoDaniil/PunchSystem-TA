a00dacb31d350b193eccd20dc5440c3ece299cbf
Malchenko Daniil <flypopa15@gmail.com>
2025-08-09 08:14:13 +0300

    Health system

---
+++ Assets/Scripts/Gameplay/Health/HealthUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    private HealthSystem _healthSystem;
+    private Slider _healthSlider;
+
+    [SerializeField] private Text _healthText;
+
+    public void Initialize(HealthSystem healthSystem)
+    {
+        _healthSystem = healthSystem;
+        _healthSystem.OnTakeDamage.AddListener(CanvasUpateHealth);
+    }
+
+    private void Start()
+    {
+        _healthSlider = GetComponent<Slider>();
+
+        _healthText.text = _healthSystem.CurrentHealth.ToString();
+        _healthSlider.maxValue = _healthSystem.CurrentHealth;
+        _healthSlider.value = _healthSystem.CurrentHealth;
+
+        CanvasUpateHealth();
+    }
+
+    private void OnDestroy()
+    {
+        if (_healthSystem != null)
+        {
+            _healthSystem.OnTakeDamage.RemoveListener(CanvasUpateHealth);
+        }
+    }
+
+    public void CanvasUpateHealth()
+    {
+        _healthText.text = _healthSystem.CurrentHealth.ToString();
+        _healthSlider.value = _healthSystem.CurrentHealth;
+    }
+}
