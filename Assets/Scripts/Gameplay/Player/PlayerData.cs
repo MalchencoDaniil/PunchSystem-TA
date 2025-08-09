8332af6cc5a81123b10a937f99049c969ae14512
Malchenko Daniil <flypopa15@gmail.com>
2025-08-09 16:07:47 +0300

    Small player data fix

--- Assets/Scripts/Gameplay/Player/PlayerData.cs
+++ Assets/Scripts/Gameplay/Player/PlayerData.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 public class PlayerData : ScriptableObject
 {
     [field: SerializeField] public float Damage = 5;
-    [field: SerializeField] public float AttackSpeed = 10;
     [field: SerializeField, Range(0.1f, 10f)] public float AttackReloadTime = 1;
 }
